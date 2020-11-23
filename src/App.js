f92ed638339f48bacebb7d93d82f68ca003c9cee
yuddmm <yuddmm@ya.ru>
2020-11-23 22:33:50 +0300

    test_2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                   <div><a href="">Friends</a></div>
                   <div><a href="">Settings</a></div>
               </nav>
-        </div>
+          </div>
       </header>
       <div className="content">
         <div className="container">
@@ -23,6 +23,66 @@ function App() {
                     <img src="https://sun9-45.userapi.com/PLejB1LEaFNFRix3xvrVHrhjQkms_LXyXtjZlw/mOShDRPMbRo.jpg" alt=""/>
                  </div>
                 <div className="fullname"><p className="name">Дмитрий Юсупов</p><p className="status">Занимаюсь херней и делаю эту социальную сеть</p></div>
+            </div>
+            <div className="info">
+                <div className="info_box">
+                    <div className="info_title">Контакты</div>
+                    <div className="info_list">
+                        <div className="info_element">
+                            <div className="info_element__key">Телефон:</div>
+                            <div className="info_element__value">+79990045426</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">E-mail:</div>
+                            <div className="info_element__value">yuddmm@yandex.ru</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">VK.COM:</div>
+                            <div className="info_element__value">@snickers0101</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Instagram:</div>
+                            <div className="info_element__value">xxdmitryzz</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Telegram:</div>
+                            <div className="info_element__value">@snickers0101</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Skype:</div>
+                            <div className="info_element__value">cs_snickers</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="info_box" id="main-info">
+                    <div className="info_title" >Основная информация</div>
+                    <div className="info_list">
+                        <div className="info_element">
+                            <div className="info_element__key">Страна:</div>
+                            <div className="info_element__value">Россия</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Город:</div>
+                            <div className="info_element__value">Санкт-Петербург</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Дата рождения:</div>
+                            <div className="info_element__value">7 ноября 1998</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Семейное положение:</div>
+                            <div className="info_element__value">не женат</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Место учебы:</div>
+                            <div className="info_element__value">СПбГАСУ</div>
+                        </div>
+                        <div className="info_element">
+                            <div className="info_element__key">Место работы:</div>
+                            <div className="info_element__value">SMOKEBBQ</div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
